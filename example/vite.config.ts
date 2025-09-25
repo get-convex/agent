692b0b2f679463e1c1b6fd9d584d4403541eb07b
Ian Macartney <ianhmacartney@gmail.com>
2025-09-24 17:54:58 -0700

    hot reload agent from source for vite

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./ui"),
+      "@convex-dev/agent": path.resolve(__dirname, "../src"),
     },
   },
 });
