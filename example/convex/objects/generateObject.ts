03399e1a735ea8cbd4195d3604f98f8f1d82f28c
Ian Macartney <ianhmacartney@gmail.com>
2025-10-09 16:40:14 -0700

    more object examples

--- example/convex/objects/generateObject.ts
+++ example/convex/objects/generateObject.ts
@@ -3,6 +3,7 @@ import { Agent, createThread } from "@convex-dev/agent";
 import { components } from "../_generated/api.js";
 import { defaultConfig } from "../agents/config.js";
 import z from "zod/v4";
+import { assert } from "convex-helpers";
 
 export const createObject = action({
   args: {},
@@ -23,6 +24,56 @@ export const createObject = action({
         }),
       },
     );
+    assert(typeof object.summary === "string");
+    for (const step of object.steps) {
+      assert(typeof step === "string");
+    }
+    return object;
+  },
+});
+
+export const createEnum = action({
+  args: {},
+  handler: async (ctx) => {
+    const agent = new Agent(components.agent, {
+      ...defaultConfig,
+      name: "object-agent",
+    });
+    const threadId = await createThread(ctx, components.agent);
+    const { object } = await agent.generateObject(
+      ctx,
+      { threadId },
+      {
+        prompt: "Generate a plan to make a sandwich",
+        output: "enum",
+        enum: ["hello", "world"],
+      },
+    );
+    assert(object === "hello" || object === "world");
+    return object;
+  },
+});
+
+export const createArray = action({
+  args: {},
+  handler: async (ctx) => {
+    const agent = new Agent(components.agent, {
+      ...defaultConfig,
+      name: "object-agent",
+    });
+    const threadId = await createThread(ctx, components.agent);
+    const { object } = await agent.generateObject(
+      ctx,
+      { threadId },
+      {
+        prompt: "Generate a plan to make a sandwich",
+        output: "array",
+        schema: z.string(),
+      },
+    );
+    for (const step of object) {
+      assert(typeof step === "string");
+    }
     return object;
   },
 });
