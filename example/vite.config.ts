090639fb5918d4534efdab671a0f3a2387a1680e
Ian Macartney <ianhmacartney@gmail.com>
2025-10-15 23:55:39 -0700

    fix example ui

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -1,14 +1,14 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
+  envDir: path.resolve(__dirname, ".."),
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./ui"),
-      "@convex-dev/agent": path.resolve(__dirname, "../src"),
     },
   },
 });
