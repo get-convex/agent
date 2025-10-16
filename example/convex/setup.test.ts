68e964bdd182940c8cd045dc623c194ab8898238
Ian Macartney <ianhmacartney@gmail.com>
2025-10-15 23:24:13 -0700

    make test better

--- example/convex/setup.test.ts
+++ example/convex/setup.test.ts
@@ -3,18 +3,15 @@ import { test } from "vitest";
 import { convexTest } from "convex-test";
 import schema from "./schema.js";
 import agent from "@convex-dev/agent/test";
+import workflow from "@convex-dev/workflow/test";
+import rateLimiter from "@convex-dev/rate-limiter/test";
 export const modules = import.meta.glob("./**/*.*s");
-
-// Sorry about everything
-import rateLimiterSchema from "../../node_modules/@convex-dev/rate-limiter/src/component/schema.js";
-const rateLimiterModules = import.meta.glob(
-  "../node_modules/@convex-dev/rate-limiter/src/component/**/*.ts",
-);
 
 export function initConvexTest() {
   const t = convexTest(schema, modules);
-  t.registerComponent("agent", agent.schema, agent.modules);
-  t.registerComponent("rateLimiter", rateLimiterSchema, rateLimiterModules);
+  agent.register(t);
+  workflow.register(t);
+  rateLimiter.register(t);
   return t;
 }
 
